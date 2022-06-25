38b2f1f1abf8924acc4525cd3120acffb858dbdf
joaodanielkm <joaodanielkm@gmail.com>
2022-06-24 22:55:19 -0300

    ADICIONADO INSTALADOR

--- WinFormsApp1/Banco.cs
+++ WinFormsApp1/Banco.cs
@@ -14,8 +14,8 @@ namespace WinFormsApp1
 
         private static SQLiteConnection ConexaoBanco()
         {
-            conexao = new SQLiteConnection("Data Source=C:\\Program Files (x86)\\Aplicativo Restaurante\\Restaurante App\\Repositorios\\Banco\\bd_restaurante.db");
-            //C:\\eRepositorios-\\dbeys\\Banco_restaurante\\
+            conexao = new SQLiteConnection("Data Source=C:\\eRepositorios-\\dbeys\\Banco_restaurante\\bd_restaurante.db");
+            //C:\\eRepositorios-\\dbeys\\Banco_restaurante\\C:\\Program Files (x86)\\Aplicativo Restaurante\\Restaurante App\\Repositorios\\Banco\\
             conexao.Open();
             return conexao;
         }
